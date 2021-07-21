271456c842f3144265cc5040a5c7042f9327b91f
Paulina Nilsson <67747928+Paulina97@users.noreply.github.com>
2021-07-21 21:22:00 +0200

    Initialize project using Create React App

---
+++ README.md
@@ -0,0 +1,3 @@
+# Create React App minimal setup
+
+This project was created with Create React App and a minimal setup custom template.
---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import CssBaseline from '@material-ui/core/CssBaseline'
+
+function App() {
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <div>
+        Hello, world!
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default App
