62c8136f52b3dc5523ea9841374a5f61e704be0f
Paulina Nilsson <67747928+Paulina97@users.noreply.github.com>
2021-07-21 21:51:34 +0200

    implement EndDialog

--- src/components/EndDialog.tsx
+++ src/components/EndDialog.tsx
@@ -1,8 +1,27 @@
 import { Dialog, DialogContent, DialogContentText, DialogActions, Button } from '@material-ui/core'
 
-function EndDialog() {
-  return (
 
+function EndDialog(props: {showDialog: boolean, score: number, bestScore: number}) {
+  return (
+    <Dialog
+        open={props.showDialog}
+        disableEscapeKeyDown
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Score: {props.score} &nbsp; 
+            Best: {props.bestScore}
+          </DialogContentText>
+        </DialogContent>
+
+        <DialogActions>
+          <Button onClick={() => window.location.reload()} color="primary">
+            Restart
+          </Button>
+        </DialogActions>
+    </Dialog>
   )
 }
 
